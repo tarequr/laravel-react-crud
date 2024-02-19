d8470dedd4ac4fadc4abd24d1061447ef6f2dc3b
tarequr <tarequrr8@gmail.com>
2024-02-19 10:46:58 +0600

    create route home and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,35 @@
 import { useState } from 'react'
+
+import './index.css'
 import "bootstrap/dist/css/bootstrap.css"
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Create from './components/pages/Create'
 
 function App() {
   return (
-    <div className='container'>
-      Hello NextGen
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav className='navbar navbar-expand navbar-dark bg-dark'>
+          <div className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link to={"/"} className='nav-link'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={"/create"} className='nav-link'>Create</Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/create' element={<Create/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/components/pages/Create.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div>Create Page</div>
+  )
+}
+
+export default Create
