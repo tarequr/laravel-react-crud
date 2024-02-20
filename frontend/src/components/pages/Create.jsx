e34ac679f962ea70f836a2920d185b41f8f84ec0
tarequr <tarequrr8@gmail.com>
2024-02-20 10:07:04 +0600

    data inserted successfully

--- frontend/src/components/pages/Create.jsx
+++ frontend/src/components/pages/Create.jsx
@@ -1,12 +1,26 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import http from '../../http'
 
 function Create() {
   const [inputs, setInputs] = useState({});
+  const navigate = useNavigate();
+
   const handelChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
     setInputs(values => ({ ...values, [name]: value }));
+  }
+
+  const submitForm = (event) => {
+    // console.log(inputs);
+    http.post('/companies',inputs)
+    .then((response) => {
+      navigate('/');
+    })
+    .catch(error => {
+      setError(error);
+    });
   }
 
   return (
@@ -16,18 +30,18 @@ function Create() {
         <h2 className='mt-5 text-center'>New Companies</h2>
             <div className='card p-3 col-sm-6 justify-content-center'>
                 <label htmlFor="name">Name</label>
-                <input type="text" name='name' id='name' className='form-control mb-2' required/>
+                <input type="text" name='name' id='name' className='form-control mb-2'  value={ inputs.name || '' } onChange={handelChange} required/>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" name='email' id='email' className='form-control mb-2' required/>
+                <input type="email" name='email' id='email' className='form-control mb-2' value={ inputs.email || '' } onChange={handelChange} required/>
 
                 <label htmlFor="address">Address</label>
-                <input type="text" name='address' id='address' className='form-control mb-2'/>
+                <input type="text" name='address' id='address' className='form-control mb-2' value={ inputs.address || '' } onChange={handelChange}/>
 
                 <label htmlFor="website">Website</label>
-                <input type="text" name='website' id='website' className='form-control mb-2'/>
+                <input type="text" name='website' id='website' className='form-control mb-2' value={ inputs.website || '' } onChange={handelChange}/>
 
-                <button type='button' className='btn btn-success mt-2'>Create</button>
+                <button type='button' onClick={submitForm} className='btn btn-success mt-2'>Create</button>
             </div>
         </div>
     </div>
